accc1bc2aed556af1d85ae8c4e31e41b7508fc93
unknown <My pc@Omidoutside-pc.satarco.local>
2026-02-22 20:19:18 +0330

    footr is left in index html

---
+++ index-js/all-books.js
@@ -0,0 +1,35 @@
+const allBooksList = document.querySelector('.all-books-list');
+const btnMore = document.querySelector('.btn-more');
+const btnLess = document.querySelector('.btn-less');
+
+let visibleCount = 4; 
+let showingAll = false;
+
+function renderBooks() {
+  const visibleBooks = showingAll ? books : books.slice(0, visibleCount);
+
+  allBooksList.innerHTML = visibleBooks.map(b => `
+    <div class="book-box">
+      <img src="${b.image}" alt="${b.title}" class="book-pic">
+      <h3 class="book-name">${b.title}</h3>
+      <p class="book-writer">${b.author}</p>
+      <p class="book-cost">${b.price} $</p>
+    </div>
+  `).join('');
+}
+
+btnMore.addEventListener('click', () => {
+  showingAll = true;
+  renderBooks();
+  btnMore.style.display = "none";
+  btnLess.style.display = "inline-block";
+});
+
+btnLess.addEventListener('click', () => {
+  showingAll = false;
+  renderBooks();
+  btnLess.style.display = "none";
+  btnMore.style.display = "inline-block";
+});
+
+renderBooks();
